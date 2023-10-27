10a5517d99ce58f47f20ad26328a82f5ab94efa1
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-10-27 05:50:11 -0400

    Added Vitals enumeration.

---
+++ source/Reoria.Game/Data/Enumerations/Vitals.cs
@@ -0,0 +1,21 @@
+﻿namespace Reoria.Game.Data.Enumerations;
+
+/// <summary>
+/// Defines flag values for <see cref="Vitals"/>.
+/// </summary>
+[Flags]
+public enum Vitals
+{
+    /// <summary>
+    /// Defines the flag value for <see cref="Health"/>.
+    /// </summary>
+    Health = 1 << 0,
+    /// <summary>
+    /// Defines the flag value for <see cref="Mana"/>.
+    /// </summary>
+    Mana = 1 << 1,
+    /// <summary>
+    /// Defines the flag value for <see cref="Stamina"/>.
+    /// </summary>
+    Stamina = 1 << 2
+}
