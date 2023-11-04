2676dc6bd8615a36e4b33fc2b3994786f71a0839
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-11-04 14:50:22 -0400

    Extracted common functionality to base class.

--- nodes/mob.gd
+++ nodes/mob.gd
@@ -1,7 +1,32 @@
 extends CharacterBody2D
 
-@export var movement_speed: int = 200
+@export var movement_speed: float = 200
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@export var state = "Idle"
-@export var direction = "Down"
+@export var state: String = "Idle"
+@export var direction: String = "Down"
+@export var input: Vector2 = Vector2.ZERO
+@export var is_sprinting: bool = false
 
+var target: Vector2 = Vector2.ZERO
+var target_tick: float = 0
+
+func update_animation() -> void:
+	state = "Idle"
+	
+	if(input.y != 0): 
+		state = "Walking" 
+		direction = "Up" if input.y < 0 else "Down"
+	elif(input.x != 0): 
+		state = "Walking" 
+		direction = "Left" if input.x < 0 else "Right"
+	
+	animation_player.play(state + "_" + direction, -1, (2 if is_sprinting else 1) )
+
+func _process(_delta):
+	pass
+
+func _physics_process(_delta):
+	velocity = input * (movement_speed / (1 if is_sprinting else 2))
+	
+	update_animation()
+	move_and_slide()
--- nodes/player.gd
+++ nodes/player.gd
@@ -1,9 +1,4 @@
-extends CharacterBody2D
-
-@export var movement_speed: int = 200
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-@export var state = "Idle"
-@export var direction = "Down"
+extends "mob.gd"
 
 func handle_player_input() -> Vector2:
 	return Vector2(
@@ -11,22 +6,6 @@ func handle_player_input() -> Vector2:
 		Input.get_axis("MOVEMENT_UP", "MOVEMENT_DOWN")
 	).normalized()
 
-func update_animation(input_vector: Vector2, is_sprinting: bool) -> void:
-	state = "Idle"
-	
-	if(input_vector.y != 0): 
-		state = "Walking" 
-		direction = "Up" if input_vector.y < 0 else "Down"
-	elif(input_vector.x != 0): 
-		state = "Walking" 
-		direction = "Left" if input_vector.x < 0 else "Right"
-	
-	animation_player.play(state + "_" + direction, -1, (2 if is_sprinting else 1) )
-
-func _physics_process(delta):
-	var is_sprinting: bool = Input.is_key_pressed(KEY_SHIFT)
-	var input: Vector2 = handle_player_input()
-	
-	velocity = input * (movement_speed / (1 if is_sprinting else 2))
-	update_animation(input, is_sprinting)
-	move_and_slide()
+func _process(_delta):
+	is_sprinting = Input.is_key_pressed(KEY_SHIFT)
+	input = handle_player_input()
