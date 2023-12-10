cf0656db8bc7d3e78fd35ef6990e7a2143d7a949
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-12-10 05:04:26 -0500

    Created ActorBase and Player classes.

---
+++ source/nodes/actors/ActorBase.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+namespace Reoria.Nodes.Actors;
+
+public partial class ActorBase : CharacterBody2D
+{
+	[Export]
+	public int TestActorValue = 25;
+}
---
+++ source/nodes/actors/player/Player.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+namespace Reoria.Nodes.Actors.Player;
+
+public partial class Player : ActorBase
+{
+	[Export]
+	public int TestPlayerValue = 50;
+}
