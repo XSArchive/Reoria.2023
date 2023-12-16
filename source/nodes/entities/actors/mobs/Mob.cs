a7b6e7418e8bf0dbba2bcfb801f5b9644c71678f
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-12-16 10:02:31 -0500

    Mobs are now assigned a random Agility value for testing.

--- source/nodes/entities/actors/mobs/Mob.cs
+++ source/nodes/entities/actors/mobs/Mob.cs
@@ -10,6 +10,9 @@ public partial class Mob : Actor
 			Constants.Input.MOVEMENT_LEFT, Constants.Input.MOVEMENT_RIGHT,
 			Constants.Input.MOVEMENT_UP, Constants.Input.MOVEMENT_DOWN) * -1;
 
+		// DEBUG: Assign random agility value for testing.
+		this.Data.Stats.Agility = new RandomNumberGenerator().RandiRange(8, 64);
+
 		base._PhysicsProcess(delta);
 	}
 }
