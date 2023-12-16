3ac5b73e2554d206f3f634a84043cfc167e38418
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-12-16 09:01:42 -0500

    Added EnumeratedType class.

---
+++ source/data/types/EnumeratedType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reoria.Data.Types;
+
+public abstract class EnumeratedType<TEnum, TValue> where TEnum : Enum
+{
+    private readonly Dictionary<TEnum, TValue> values;
+
+    public EnumeratedType() => this.values = new Dictionary<TEnum, TValue>();
+    public EnumeratedType(Dictionary<TEnum, TValue> values) => this.values = values;
+
+    public EnumeratedType<TEnum, TValue> Populate()
+    {
+        foreach (TEnum key in Enum.GetValues(typeof(TEnum)))
+        {
+            this.values.Add(key, default);
+        }
+
+        return this;
+    }
+
+    public virtual TValue GetValue(TEnum key, TValue defaultValue = default) => !this.values.ContainsKey(key) ? defaultValue : this.values[key];
+
+    public virtual void SetValue(TEnum key, TValue value)
+    {
+        if (!this.values.ContainsKey(key)) { this.values.Add(key, value); }
+
+        this.values[key] = value;
+    }
+}
