1679f53674d17ef418c46e8b45a7079dfbd83a37
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2024-01-08 01:12:10 -0500

    Added singleton pattern to GameContainer.

--- source/Reoria.Godot/Nodes/Core/Camera.cs
+++ source/Reoria.Godot/Nodes/Core/Camera.cs
@@ -9,23 +9,23 @@ namespace Reoria.Godot.Nodes.Core;
 /// </summary>
 public partial class Camera : Camera2D, IOwnerPattern<GameContainer>
 {
-    /// <summary>
-    /// Defines the reference to the owner of this <see cref="Node"/>.
-    /// </summary>
-    public new GameContainer? Owner { get; protected set; }
+	/// <summary>
+	/// Defines the reference to the owner of this <see cref="Node"/>.
+	/// </summary>
+	public new GameContainer? Owner { get; protected set; }
 
-    /// <summary>
-    /// Called when the node is "ready", i.e. when both the node and its children have
-    /// entered the scene tree. If the node has children, their <see cref="Node._Ready"/> 
-    /// callbacks get triggered first, and the parent node will receive the ready 
-    /// notification afterwards.
-    /// </summary>
-    public override void _Ready()
-    {
-        // Fetch and assign the owner node variable.
-        this.Owner = this.GetOwner<GameContainer>();
+	/// <summary>
+	/// Called when the node is "ready", i.e. when both the node and its children have
+	/// entered the scene tree. If the node has children, their <see cref="Node._Ready"/> 
+	/// callbacks get triggered first, and the parent node will receive the ready 
+	/// notification afterwards.
+	/// </summary>
+	public override void _Ready()
+	{
+		// Fetch and assign the owner node variable.
+		this.Owner = this.GetOwner<GameContainer>();
 
-        // Pass to the base class' function.
-        base._Ready();
-    }
+		// Pass to the base class' function.
+		base._Ready();
+	}
 }
--- source/Reoria.Godot/Scenes/GameContainer.cs
+++ source/Reoria.Godot/Scenes/GameContainer.cs
@@ -9,10 +9,44 @@ namespace Reoria.Godot.Scenes;
 public partial class GameContainer : Node
 {
 	/// <summary>
+	/// The reference to the root <see cref="Node"/>.
+	/// </summary>
+	private static GameContainer? instance;
+
+	/// <summary>
+	/// The reference to the root <see cref="Node"/>.
+	/// </summary>
+	public static GameContainer? Instance
+	{
+		get
+		{
+			// Check to see if the root node is assigned, and return it if it has, if it has not then close the game.
+			return instance is not null ? instance
+				: throw new NullReferenceException("The game's root node has been lost, the game will now close.");
+		}
+	}
+
+	/// <summary>
 	/// Defines the reference to the <see cref="Nodes.Core.Camera"/>.
 	/// </summary>
 	[Export]
 	public Camera? Camera { get; protected set; }
+
+	/// <summary>
+	/// Constructs a new <see cref="GameContainer"/> instance and assignes the reference to it to <see cref="Instance"/>.
+	/// </summary>
+	public GameContainer()
+	{
+		// Check to see if the root node has already been created.
+		if (instance is not null)
+		{
+			// Yes it has, we can not allow "Dave" to do this.
+			throw new InvalidOperationException("The game can not load more then one root node, the game will now close.");
+		}
+
+		// Assign this instance of the root node to the instance variable.
+		instance = this;
+	}
 
 	/// <summary>
 	/// Called when the node is "ready", i.e. when both the node and its children have
