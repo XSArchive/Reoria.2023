43ba45613170952743f12ab1aa54d538fe06d1ea
XerShade <xershade.ca@gmail.com>
2024-01-10 17:48:07 -0500

    Added base entity types.

---
+++ source/Reoria.Godot/Nodes/Entities/StaticEntity.cs
@@ -0,0 +1,32 @@
+using Godot;
+using Reoria.Godot.Nodes.Entities.Interfaces;
+
+namespace Reoria.Godot.Nodes.Entities;
+
+/// <summary>
+/// Defines the properties and functions of entity nodes that inherit from <see cref="StaticBody2D"/>.
+/// </summary>
+public partial class StaticEntity : StaticBody2D, IEntity
+{
+	/// <summary>
+	/// A node that provides a <see cref="Shape2D"/> to a <see cref="CollisionObject2D"/> parent and
+	/// allows to edit it. This can give a detection shape to an <see cref="Area2D"/> or turn
+	/// a <see cref="PhysicsBody2D"/> into a solid object.
+	/// </summary>
+	public CollisionShape2D? CollisionShape { get; protected set; }
+
+	/// <summary>
+	/// Called when the node is "ready", i.e. when both the node and its children have
+	/// entered the scene tree. If the node has children, their <see cref="Node._Ready"/> 
+	/// callbacks get triggered first, and the parent node will receive the ready 
+	/// notification afterwards.
+	/// </summary>
+	public override void _Ready()
+	{
+		// Fetch and assign the owner node variable.
+		this.CollisionShape = this.GetNode<CollisionShape2D?>("CollisionShape");
+
+		// Pass to the base class' function.
+		base._Ready();
+	}
+}
