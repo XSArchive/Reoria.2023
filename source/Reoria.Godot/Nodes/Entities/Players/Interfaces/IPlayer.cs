baba2c14ef1c4dc0692669cf392af6c2d75b387f
XerShade <xershade.ca@gmail.com>
2024-01-16 08:17:16 -0500

    Created Player node.

---
+++ source/Reoria.Godot/Nodes/Entities/Players/Interfaces/IPlayer.cs
@@ -0,0 +1,11 @@
+﻿using Godot;
+using Reoria.Godot.Nodes.Entities.Interfaces;
+
+namespace Reoria.Godot.Nodes.Entities.Players.Interfaces;
+
+/// <summary>
+/// Provides a common properties and functions for <see cref="Player"/> <see cref="Node"/>s.
+/// </summary>
+public interface IPlayer : IKinematicEntity
+{
+}
---
+++ source/Reoria.Godot/Nodes/Entities/Players/Player.cs
@@ -0,0 +1,8 @@
+﻿using Reoria.Godot.Nodes.Entities.Players.Interfaces;
+
+namespace Reoria.Godot.Nodes.Entities.Players;
+
+public partial class Player : KinematicEntity, IPlayer
+{
+
+}
