8327d8f511cf124bce97038960e158c16de0fc45
XerShade <xershade.ca@gmail.com>
2024-01-16 08:35:04 -0500

    Added INodeWithSprite interface.

---
+++ source/Reoria.Godot/Nodes/Interfaces/INodeWithSprite.cs
@@ -0,0 +1,15 @@
+﻿using Godot;
+
+namespace Reoria.Godot.Nodes.Interfaces;
+
+/// <summary>
+/// Provides a common properties and functions for a <see cref="Node"/> with a <see cref="Sprite2D"/>.
+/// </summary>
+public interface INodeWithSprite
+{
+    /// <summary>
+    /// A node that displays a 2D texture. The texture displayed can be a region from
+    /// a larger atlas texture, or a frame from a sprite sheet animation.
+    /// </summary>
+    Sprite2D Sprite { get; }
+}
