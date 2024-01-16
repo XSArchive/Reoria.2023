c022cc18e01f6d2c5a5f4ed4be5c001d30d0b91e
XerShade <xershade.ca@gmail.com>
2024-01-16 08:36:18 -0500

    Added INodeWithCollisionShape interface.

--- source/Reoria.Godot/Nodes/Entities/Interfaces/IEntity.cs
+++ source/Reoria.Godot/Nodes/Entities/Interfaces/IEntity.cs
@@ -1,16 +1,11 @@
 ﻿using Godot;
+using Reoria.Godot.Nodes.Interfaces;
 
 namespace Reoria.Godot.Nodes.Entities.Interfaces;
 
 /// <summary>
 /// Provides a common properties and functions for Entity <see cref="Node"/>s.
 /// </summary>
-public interface IEntity
+public interface IEntity : INodeWithCollisionShape
 {
-    /// <summary>
-    /// A node that provides a <see cref="Shape2D"/> to a <see cref="CollisionObject2D"/> parent and
-    /// allows to edit it. This can give a detection shape to an <see cref="Area2D"/> or turn
-    /// a <see cref="PhysicsBody2D"/> into a solid object.
-    /// </summary>
-    CollisionShape2D? CollisionShape { get; }
 }
---
+++ source/Reoria.Godot/Nodes/Interfaces/INodeWithCollisionShape.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace Reoria.Godot.Nodes.Interfaces;
+
+/// <summary>
+/// Provides a common properties and functions for a <see cref="Node"/> with a <see cref="CollisionShape2D"/>.
+/// </summary>
+public interface INodeWithCollisionShape
+{
+    /// <summary>
+    /// A node that provides a <see cref="Shape2D"/> to a <see cref="CollisionObject2D"/> parent and
+    /// allows to edit it. This can give a detection shape to an <see cref="Area2D"/> or turn
+    /// a <see cref="PhysicsBody2D"/> into a solid object.
+    /// </summary>
+    CollisionShape2D? CollisionShape { get; }
+}
